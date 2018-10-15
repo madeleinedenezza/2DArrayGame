524fe88da255599be73b5a1f24deea55c8957513
Madeleine.denezza <Madeleine.denezza@MU-C1-10.mullen.pvt.k12.co.us>
2018-10-15 15:19:52 -0600

    add edit array list

--- 2D Array Game/src/TriviaDatabase.java
+++ 2D Array Game/src/TriviaDatabase.java
@@ -14,7 +14,7 @@ public class TriviaDatabase
 		questions.add(new Trivia("Easy", 1, 2, 3, "What is the name of the fairy in Peter Pan? \na The Fairy Godmother \nb Tinkerbell \nc Snow White \nd Puck", "b"));
 		questions.add(new Trivia("Easy", 1, 2, 4, "What color is the circle on the Japanese national flag? \na Orange \nb Green \nc Brown \nd Red", "d"));
 		questions.add(new Trivia("Easy", 1, 3, 1, "Which fruit is the most popular and most consumed fruit in the world? \na Oranges \nb Bananas \nc Apples \nd Kiwi", "b"));
-		questions.add(new Trivia("Easy", 1, 3, 2, "What NFL team has the most Superbowl wins? \na The Pittsburgh Steelers \nb The New England Patriots \nc The Detroit Lions \nc The Denver Broncos", "a"));
+		questions.add(new Trivia("Easy", 1, 3, 2, "What NFL team has the most Superbowl wins? \na The Pittsburgh Steelers \nb The New England Patriots \nc The Detroit Lions \nd The Denver Broncos", "a"));
 		questions.add(new Trivia("Easy", 1, 3, 3, "What country is home to the Great Barrier Reef? \na Brazil \nb Italy \nc Australia \nd South Africa", "c"));
 		questions.add(new Trivia("Easy", 1, 3, 4, "What movie features a cowboy named Woody? \na Star Wars \nb Fast and Furious \nc Wall-E \nd Toy Story", "d"));
 		questions.add(new Trivia("Easy", 1, 4, 1, "Who invented the light bulb? \na Thomas Jefferson \nb Andrew Carnegie \nc Thomas Eddison \nd Cornelius Vanderbilt", "c"));
@@ -26,6 +26,8 @@ public class TriviaDatabase
 		questions.add(new Trivia("Difficult", 2, 1, 3, "Which gas is formed when a hydrogen bomb is detonated?", "Helium"));
 		questions.add(new Trivia("Difficult", 2, 1, 4, "Who holds the record for the most homeruns in a single major league baseball season?", "Barry Bonds"));
 		questions.add(new Trivia("Difficult", 2, 2, 1, "The expression 'oy vey' comes from what language?", "Yiddish"));
-		questions.add(new Trivia("Difficult", 2, 2, 2, ""))
+		questions.add(new Trivia("Difficult", 2, 2, 2, "What is the scientific name for the trunk of an elephant?", "Proboscis"));
+		questions.add(new Trivia("Difficult", 2, 2, 3, ""));
 		
+		}
 		}
